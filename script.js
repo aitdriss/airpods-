dd68130ce012c60aac6b4247280da473b4fee256
aitdriss <60844162+aitdriss@users.noreply.github.com>
2025-11-30 18:35:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function change(){
+        b = document.getElementById('bb');
+        l = b.style.backgroundColor = 'Pink';
+}
+
+function change2(){
+    h = document.getElementById('bb');
+    j = h.style.backgroundColor = 'skyblue';
+}
+
+
